ea0af13877d6c1182aa139566492da9a53d099c2
liugang <544241065@qq.com>
2017-03-05 16:24:11 +0800

    Update drag.js
    更新移动端拖拽效果

--- drag.js
+++ drag.js
@@ -1,9 +1,46 @@
-
-function drag(id){
-	var obj = document.getElementById(id);
-	var disX = 0;
-	var disY = 0;
-	obj.onmousedown = function (ev) {
-		var ev = ev || window.event;
-	}
+function drag(obj,parentNode){
+    var obj = document.getElementById(obj);
+    if(arguments.length == 1){
+        var parentNode = window.self;  
+        var pWidth = parentNode.innerWidth,pHeight = parentNode.innerHeight;   
+    }else{
+        var parentNode = document.getElementById(parentNode);
+        var pWidth = parentNode.clientWidth,pHeight = parentNode.clientHeight;
+    }
+    obj.addEventListener('touchstart',function(event){
+        //当只有一个手指时              .
+        if(event.touches.length == 1){
+            //禁止浏览器默认事
+            event.preventDefault();
+        };
+        var touch = event.targetTouches[0];
+        var disX = touch.clientX - obj.offsetLeft,disY = touch.clientY - obj.offsetTop;
+        var oWidth = obj.offsetWidth,oHeight = obj.offsetHeight;
+ 
+        obj.addEventListener('touchmove',function(event){
+            var touch = event.targetTouches[0];
+            obj.style.left = touch.clientX - disX  + 'px';
+            obj.style.top = touch.clientY - disY + 'px';
+            //左侧
+            if(obj.offsetLeft <=0){
+                obj.style.left = 0;
+            };
+            //右侧
+            if(obj.offsetLeft >= pWidth -oWidth){
+                obj.style.left =  pWidth - oWidth + 'px';  
+            };
+            //上面
+            if(obj.offsetTop <= 0){
+                obj.style.top = 0; 
+            };
+            //下面
+            if(obj.offsetTop >= pHeight - oHeight){
+                obj.style.top =  pHeight - oHeight + 'px'; 
+            };                 
+        });
+        obj.addEventListener('touchend',function(event){
+            obj.removeEventListener('touchmove');
+            obj.removeEventListener('touchend');
+        })
+    });
 }
